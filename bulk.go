765bbacec93369c041fdd7684d2f5af57b49a7fa
Adam Sitavanc <work@sitavanc.cz>
2025-01-14 09:53:30 +0100

    bulk

---
+++ bulk.go
@@ -0,0 +1,123 @@
+package search
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/mailstepcz/serr"
+	"github.com/opensearch-project/opensearch-go/v4"
+	"github.com/opensearch-project/opensearch-go/v4/opensearchapi"
+)
+
+var (
+	// ErrBulkItemError fails if there was an error in the bulk response.
+	ErrBulkItemError = errors.New("bulk item error")
+)
+
+type BulkOperationType string
+
+const (
+	OpCreate BulkOperationType = "create"
+	OpDelete BulkOperationType = "delete"
+	OpIndex  BulkOperationType = "index"
+	OpUpdate BulkOperationType = "update"
+)
+
+type BulkOperation[T any] struct {
+	OperationType BulkOperationType `json:"-"`
+	ID            string            `json:"id"`
+	Index         string            `json:"index"`
+	Doc           *T                `json:"doc"`
+}
+
+// Bulk sends a bulk request with the specified ops.
+func Bulk[T any](ctx context.Context, cl *opensearch.Client, index string, docs []BulkOperation[T]) error {
+	return bulk(ctx, cl, index, docs, nil)
+}
+
+// BulkWithRefresh sends a bulk request with the specified ops and refresh = true parameter.
+// https://opensearch.org/docs/latest/api-reference/document-apis/bulk/#query-parameters
+func BulkWithRefresh[T any](ctx context.Context, cl *opensearch.Client, index string, docs []BulkOperation[T]) error {
+	return bulk(ctx, cl, index, docs, &opensearchapi.BulkParams{Refresh: "true"})
+}
+
+func bulk[T any](ctx context.Context, cl *opensearch.Client, index string, ops []BulkOperation[T], params *opensearchapi.BulkParams) error {
+	b, err := buildBulkBody(ops)
+	if err != nil {
+		return err
+	}
+
+	req := opensearchapi.BulkReq{
+		Index: index,
+		Body:  bytes.NewReader(b),
+	}
+
+	if params != nil {
+		req.Params = *params
+	}
+
+	var bulkResponse opensearchapi.BulkResp
+	resp, err := cl.Do(ctx, req, &bulkResponse)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.IsError() {
+		return osError(resp)
+	}
+
+	if bulkResponse.Errors {
+		var errs error
+		for _, items := range bulkResponse.Items {
+			for _, item := range items {
+				if item.Error == nil {
+					continue
+				}
+
+				errors.Join(errs, serr.Wrap(
+					"",
+					ErrBulkItemError,
+					serr.String("type", item.Error.Type),
+					serr.String("reason", item.Error.Reason),
+					serr.String("causeType", item.Error.Cause.Type),
+					serr.String("causeReason", item.Error.Cause.Reason),
+				))
+			}
+		}
+		return errs
+	}
+
+	return nil
+}
+
+func buildBulkBody[T any](ops []BulkOperation[T]) ([]byte, error) {
+	var buffer bytes.Buffer
+
+	for _, op := range ops {
+		metaBytes, err := json.Marshal(map[string]interface{}{
+			string(op.OperationType): map[string]string{
+				"_index": op.Index,
+				"_id":    op.ID,
+			},
+		})
+		if err != nil {
+			return nil, serr.Wrap("marshalling meta JSON", err, serr.String("index", op.Index), serr.String("id", op.ID))
+		}
+		buffer.Write(metaBytes)
+		buffer.WriteByte('\n')
+
+		if op.OperationType != OpDelete && op.Doc != nil {
+			docBytes, err := json.Marshal(op.Doc)
+			if err != nil {
+				return nil, serr.Wrap("marshalling document JSON", err, serr.String("operationType", string(op.OperationType)))
+			}
+
+			buffer.Write(docBytes)
+			buffer.WriteByte('\n')
+		}
+	}
+
+	return bytes.TrimSuffix(buffer.Bytes(), []byte("\n")), nil
+}
---
+++ bulk_test.go
@@ -0,0 +1,100 @@
+package search
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_buildBulkBody(t *testing.T) {
+	type testCase[T any] struct {
+		name string
+		ops  []BulkOperation[T]
+		want []byte
+	}
+
+	type doc struct {
+		Id   string `json:"id"`
+		Name string `json:"name"`
+	}
+
+	tests := []testCase[doc]{{
+		name: "2 index ops",
+		ops: []BulkOperation[doc]{
+			{
+				OperationType: OpIndex,
+				ID:            "ID-1",
+				Index:         "test-index",
+				Doc: &doc{
+					Id:   "1",
+					Name: "Doc 1",
+				},
+			}, {
+				OperationType: OpIndex,
+				ID:            "ID-2",
+				Index:         "test-index",
+				Doc: &doc{
+					Id:   "2",
+					Name: "Doc 2",
+				},
+			},
+		},
+		want: []byte(`{"index":{"_id":"ID-1","_index":"test-index"}}
+{"id":"1","name":"Doc 1"}
+{"index":{"_id":"ID-2","_index":"test-index"}}
+{"id":"2","name":"Doc 2"}`),
+	}, {
+		name: "index and delete",
+		ops: []BulkOperation[doc]{
+			{
+				OperationType: OpIndex,
+				ID:            "ID-1",
+				Index:         "test-index",
+				Doc: &doc{
+					Id:   "1",
+					Name: "Doc 1",
+				},
+			}, {
+				OperationType: OpDelete,
+				ID:            "ID-2",
+				Index:         "test-index",
+			},
+		},
+		want: []byte(`{"index":{"_id":"ID-1","_index":"test-index"}}
+{"id":"1","name":"Doc 1"}
+{"delete":{"_id":"ID-2","_index":"test-index"}}`),
+	}, {
+		name: "index",
+		ops: []BulkOperation[doc]{
+			{
+				OperationType: OpIndex,
+				ID:            "ID-1",
+				Index:         "test-index",
+				Doc: &doc{
+					Id:   "1",
+					Name: "Doc 1",
+				},
+			},
+		},
+		want: []byte(`{"index":{"_id":"ID-1","_index":"test-index"}}
+{"id":"1","name":"Doc 1"}`),
+	}, {
+		name: "delete",
+		ops: []BulkOperation[doc]{
+			{
+				OperationType: OpDelete,
+				ID:            "ID-2",
+				Index:         "test-index",
+			},
+		},
+		want: []byte(`{"delete":{"_id":"ID-2","_index":"test-index"}}`),
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := require.New(t)
+			got, err := buildBulkBody(tt.ops)
+
+			req.NoError(err)
+			req.Equal(tt.want, got)
+		})
+	}
+}
